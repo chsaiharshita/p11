ec714039ef76dc4167e55ad7cb317e597daa3552
raheema <rahimashaik825@gmail.com>
2025-08-29 16:26:05 +0530

    updated

--- src/menus/C600/map.js
+++ src/menus/C600/map.js
@@ -3,42 +3,49 @@ import { Map, TileLayer, Marker, Popup } from "react-leaflet";
 import L from "leaflet";
 import "./map.css";
 import "leaflet/dist/leaflet.css";
-import imgmap from "../../images/marker.png";
+import siteData from "../../sitedata.json";  // <-- load icons here
 import cities from "../../cities.json";
 
-const markerIcon = new L.Icon({
-  iconUrl: imgmap,
-  iconSize: [40, 40],
-  iconAnchor: [17, 46], //[left/right, top/bottom]
-  popupAnchor: [0, -46], //[left/right, top/bottom]
-});
-
 const Contactmap = () => {
+  const markerIcons = siteData.mapIcons || [];  // <-- array of marker images
+
   return (
-    <>
-      <div className="row">
-        <div className="col">
-          {cities.map((city, idx) => (
-            <Map center={city.center} zoom={city.zoomlevel}>
+    <div className="row">
+      <div className="col">
+        {cities.map((city, idx) => {
+          // Create dynamic marker icon for each city
+          const iconUrl = markerIcons[idx] ? `/${markerIcons[idx]}` : "/default-marker.png";
+          const dynamicIcon = new L.Icon({
+            iconUrl,
+            iconSize: [40, 40],
+            iconAnchor: [17, 46],
+            popupAnchor: [0, -46],
+          });
+
+          return (
+            <Map 
+              key={idx}   // <-- React needs this
+              center={city.center} 
+              zoom={city.zoomlevel}
+            >
               <TileLayer
                 url="https://api.maptiler.com/maps/basic/256/{z}/{x}/{y}.png?key=rurdVhluzyQimIQ3liOr"
                 attribution='&copy; <a href="https://www.maptiler.com/">MapTiler</a> &copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
               />
               <Marker
                 position={[city.lat, city.lng]}
-                icon={markerIcon}
-                key={idx}
+                icon={dynamicIcon}  // <-- dynamic icon here
               >
                 <Popup>
                   <b>{city.name},</b> <br />
-                  {city.address} 
+                  {city.address}
                 </Popup>
               </Marker>
             </Map>
-          ))}
-        </div>
+          );
+        })}
       </div>
-    </>
+    </div>
   );
 };
 
